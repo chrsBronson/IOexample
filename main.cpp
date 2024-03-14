a43031b47da0eb1daf59a7c24140a6405dc0204e
chrsBronson <yzharko@ya.ru>
2024-03-14 20:38:14 +0300

    simple input for some structure

--- main.cpp
+++ main.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
+namespace example
+{
+  struct SomeStruct
+  {
+  public:
+    SomeStruct(int a, int b):
+      a_(a),
+      b_(b)
+    {}
+    int getA() const
+    {
+      return a_;
+    }
+    int getB() const
+    {
+      return b_;
+    }
+  private:
+    int a_, b_;
+  };
+  std::istream & operator>>(std::istream & is, SomeStruct & value)
+  {
+    std::istream::sentry guard(is);
+    if(!guard)
+    {
+      return is;
+    }
+    int a = 0, b = 0;
+    is >> a >> b;
+    if (is)
+    {
+      value = SomeStruct(a, b);
+    }
+    return is;
+  }
+  std::ostream & operator<<(std::ostream & out, const SomeStruct & value)
+  {
+    std::ostream::sentry guard(out);
+    if(!guard)
+    {
+      return out;
+    }
+    out << value.getA() << " " << value.getB();
+    return out;
+  }
+}
 int main()
 {
-  int a = 0, b = 0;
-  if(!(std::cin >> a >> b))
-  {
-    std::cin.clear();
-    std::cin.ignore(1);
-    if(!(std::cin >> a >> b))
-    {
-      std::cerr << "Error\n";
-      return 1;
-    }
-  }
-  std::cout << a << " " << b << "\n";
+  example::SomeStruct newStruct(0, 0);
+  std::cin >> newStruct;
+  std::cout << newStruct << "\n";
   return 0;
 }
