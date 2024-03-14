a9c4b46af9d28db2c8894ac39af4ae916448dd93
chrsBronson <yzharko@ya.ru>
2024-03-14 20:00:48 +0300

    trying to recover

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 int main()
 {
   int a = 0, b = 0;
-  std::cin >> a >> b;
-  if(std::cin.fail())
+  if(!(std::cin >> a >> b))
   {
-    std::cerr << "Error\n";
-    return 1;
+    std::cin.clear();
+    std::cin.ignore(1);
+    if(!(std::cin >> a >> b))
+    {
+      std::cerr << "Error\n";
+      return 1;
+    }
   }
   std::cout << a << " " << b << "\n";
   return 0;
